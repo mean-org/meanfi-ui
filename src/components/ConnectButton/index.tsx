5db415e7d478a771a3dfa76f556eeb1a87866e0f
Yamel Amador Fernandez <yamel_amador_fdez@msn.com>
2021-05-29 15:52:52 -0500

    Wallet connect to go manual (manual workflow works well while auto-connect flow is buggy)

--- src/components/ConnectButton/index.tsx
+++ src/components/ConnectButton/index.tsx
@@ -3,17 +3,27 @@ import { LABELS } from "../../constants";
 import { useWallet } from "../../contexts/wallet";
 
 export const ConnectButton = () => {
-  const { wallet, connected, provider, lastWalletProviderSuccess, select, connect } = useWallet();
+  const { connected, select } = useWallet();
 
-  if (wallet && !connected && provider?.url === lastWalletProviderSuccess) {
-    return (
-      <Button type="primary" shape="round" size="large" onClick={connect}>
-        {LABELS.CONNECT_LABEL}
-      </Button>
-    );
-  }
+  // const { wallet, connected, provider, lastWalletProviderSuccess, select, connect } = useWallet();
+
+  // if (wallet && !connected && provider?.url === lastWalletProviderSuccess) {
+  //   return (
+  //     <Button type="primary" shape="round" size="large" onClick={connect}>
+  //       {LABELS.CONNECT_LABEL}
+  //     </Button>
+  //   );
+  // }
   
-  if (!provider || !connected) {
+  // if (!provider || !connected) {
+  //   return (
+  //     <Button type="primary" shape="round" size="large" onClick={select}>
+  //       {LABELS.CONNECT_LABEL}
+  //     </Button>
+  //   );
+  // }
+
+  if (!connected) {
     return (
       <Button type="primary" shape="round" size="large" onClick={select}>
         {LABELS.CONNECT_LABEL}
@@ -22,4 +32,5 @@ export const ConnectButton = () => {
   }
 
   return null;
+
 };
