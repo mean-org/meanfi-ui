ac8872ba2dc0cb566147ab4e7435205fda76ece7
Yamel Amador Fernandez <yamel_amador_fdez@msn.com>
2021-08-25 21:11:39 -0500

    Remove Wrap

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -3,5 +3,4 @@ export { FaucetView } from "./faucet";
 export { HomeView } from "./home";
 export { SwapView } from './swap';
 export { TransfersView } from './transfers';
-export { WrapView } from './wrap';
 export { PayrollView } from './payroll';
