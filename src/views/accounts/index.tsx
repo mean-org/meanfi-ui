0a9719a162a41d1c2c154bf2da027589cba4b724
Yamel Amador Fernandez <yamel_amador_fdez@msn.com>
2021-09-02 07:32:06 -0500

    Lazy-get Txs asynchronously

--- src/views/accounts/index.tsx
+++ src/views/accounts/index.tsx
@@ -1,106 +1,98 @@
-import React from 'react';
+import React, { useContext, useReducer } from 'react';
 import { CheckCircleOutlined, SyncOutlined } from '@ant-design/icons';
-import { Connection } from '@solana/web3.js';
+import { ConfirmedSignatureInfo, Connection } from '@solana/web3.js';
 import { useCallback, useEffect, useState } from 'react';
 import { PreFooter } from '../../components/PreFooter';
 import { TransactionItemView } from '../../components/TransactionItemView';
 import { useConnectionConfig } from '../../contexts/connection';
 import { useWallet } from '../../contexts/wallet';
 import { TransactionWithSignature } from '../../utils/transactions';
-import { TransactionStats } from '../../models/transactions';
-
-const defaultTransactionStats = new TransactionStats();
+import {
+  ActionTypes, defaultTransactionStats, IncrementTransactionIndexAction,
+  ResetStatsAction, MoveTxIndexToEndAction, SetStatsAction, TransactionActions, TransactionStats
+} from '../../models/transactions';
+import { AppStateContext } from '../../contexts/appstate';
 
 export const AccountsView = () => {
   const connection = useConnectionConfig();
-  const { publicKey } = useWallet();
+  const { publicKey, connected } = useWallet();
   const [customConnection, setCustomConnection] = useState<Connection>();
-  const [shouldLoadTxs, setShouldLoadTxs] = useState(true);
+  const { previousWalletConnectState } = useContext(AppStateContext);
+
+  // Flow control
+  const [shouldGetTxDetails, setShouldGetTxDetails] = useState(false);
   const [loadingTransactions, setLoadingTransactions] = useState(false);
   const [abortSignalReceived, setAbortSignalReceived] = useState(false);
+
+  // Data
+  const [signatures, setSignatures] = useState<Array<ConfirmedSignatureInfo>>([]);
   const [transactions, setTransactions] = useState<Array<TransactionWithSignature>>([]);
+  const [stats, dispatch] = useReducer((state: TransactionStats, action: TransactionActions) => {
+    switch (action.type) {
+      case ActionTypes.SET_STATS:
+        return {...state, ...action.payload};
+      case ActionTypes.RESET_STATS:
+        return {...state, ...defaultTransactionStats};
+      case ActionTypes.ROLL_INDEX:
+        return Object.assign({}, state, { index: signatures.length - 1 });
+      case ActionTypes.INCREMENT_INDEX:
+        return Object.assign({}, state, { index: state.index + 1 });
+      default:
+        return state;
+    }
+  }, defaultTransactionStats);
 
-  const [transactionStats, setTransactionStats] = useState<TransactionStats>(defaultTransactionStats);
+  // Methods
+  const abortSwitch = () => {
+    setAbortSignalReceived(abortSignalReceived => !abortSignalReceived);
+    setShouldGetTxDetails(false);
+    setLoadingTransactions(false);
+    dispatch(new MoveTxIndexToEndAction());
+  }
 
-  const abortSwitch = () => setAbortSignalReceived(abortSignalReceived => !abortSignalReceived);
-
-  const loadTransactionSignatures = useCallback(async () => {
-
-    if (!shouldLoadTxs) { return; }
+  const loadTransactionSignatures = useCallback(() => {
 
     if (customConnection && publicKey && !loadingTransactions) {
       setLoadingTransactions(true);
-      const transSignatures = await customConnection.getConfirmedSignaturesForAddress2(publicKey);
-      console.log('transSignatures:', transSignatures);
-
-      const transactions = new Array<TransactionWithSignature>();
-
-      for (const item of transSignatures) {
-        console.log('abortSignalReceived:', abortSignalReceived);
-        if (abortSignalReceived) {
-          setLoadingTransactions(false);
-          return;
-        } else {
-          const signature = item.signature;
-          const confirmedTransaction = await customConnection.getConfirmedTransaction(signature);
-          if (confirmedTransaction) {
-            const transWithSignature = new TransactionWithSignature(
-              signature,
-              confirmedTransaction
-            );
-            transactions.push(transWithSignature);
-            setTransactions(transactions => [...transactions, transWithSignature]);
+      customConnection.getConfirmedSignaturesForAddress2(publicKey)
+        .then(sigs => {
+          setSignatures(sigs);
+          const newStats = new TransactionStats();
+          newStats.index = 0;
+          newStats.total = sigs.length;
+          dispatch(new SetStatsAction(newStats));
+          console.log('transSignatures:', signatures);
+          console.log('stats:', newStats);
+          if (sigs.length > 0) {
+            setShouldGetTxDetails(true);
+          } else {
+            setTransactions([]);
+            dispatch(new ResetStatsAction());
+            setLoadingTransactions(false);
           }
-        }
-      }
+        })
+        .catch(error => {
+          console.error(error.message, error);
+          setSignatures([]);
+          setShouldGetTxDetails(false);
+          dispatch(new ResetStatsAction());
+          setLoadingTransactions(false);
+        });
+    }
+
+    // Cleanup
+    return () => {
       setLoadingTransactions(false);
+      setSignatures([]);
+      setTransactions([]);
+      dispatch(new ResetStatsAction());
     }
   }, [
-    abortSignalReceived,
-    shouldLoadTxs,
+    stats,
     customConnection,
     loadingTransactions,
     publicKey
   ]);
-
-  // const loadTransactions = useCallback(async () => {
-
-  //   if (!shouldLoadTxs) { return; }
-
-  //   if (customConnection && publicKey && !loadingTransactions) {
-  //     setLoadingTransactions(true);
-  //     const transSignatures = await customConnection.getConfirmedSignaturesForAddress2(publicKey);
-  //     console.log('transSignatures:', transSignatures);
-
-  //     const transactions = new Array<TransactionWithSignature>();
-
-  //     for (const item of transSignatures) {
-  //       console.log('abortSignalReceived:', abortSignalReceived);
-  //       if (abortSignalReceived) {
-  //         setLoadingTransactions(false);
-  //         return;
-  //       } else {
-  //         const signature = item.signature;
-  //         const confirmedTransaction = await customConnection.getConfirmedTransaction(signature);
-  //         if (confirmedTransaction) {
-  //           const transWithSignature = new TransactionWithSignature(
-  //             signature,
-  //             confirmedTransaction
-  //           );
-  //           transactions.push(transWithSignature);
-  //           setTransactions(transactions => [...transactions, transWithSignature]);
-  //         }
-  //       }
-  //     }
-  //     setLoadingTransactions(false);
-  //   }
-  // }, [
-  //   abortSignalReceived,
-  //   shouldLoadTxs,
-  //   customConnection,
-  //   loadingTransactions,
-  //   publicKey
-  // ]);
 
   // First load
   useEffect(() => {
@@ -108,15 +100,109 @@ export const AccountsView = () => {
       setCustomConnection(new Connection(connection.endpoint, 'confirmed'));
     }
 
-    if (shouldLoadTxs && customConnection && publicKey) {
+    // auto execute if wallet already connected
+    if (customConnection && publicKey) {
       setAbortSignalReceived(false);
-      setShouldLoadTxs(false);
       loadTransactionSignatures();
     }
   }, [
     connection.endpoint,
+    customConnection
+  ]);
+
+  // Hook on the wallet connect/disconnect
+  useEffect(() => {
+    if (previousWalletConnectState !== connected) {
+      if (!previousWalletConnectState && connected) {
+        console.log('Fetching account stats...');
+        if (customConnection) {
+          setAbortSignalReceived(false);
+          loadTransactionSignatures();
+        }
+      } else if (previousWalletConnectState && !connected) {
+        console.log('Deactivating account stats...');
+        setAbortSignalReceived(true);
+        setShouldGetTxDetails(false);
+        setLoadingTransactions(false);
+      }
+    }
+  }, [
+    connected,
     customConnection,
-    publicKey
+    previousWalletConnectState
+  ]);
+
+  // Get transaction detail for each signature if not already loaded
+  useEffect(() => {
+
+    if (shouldGetTxDetails && customConnection && publicKey && !abortSignalReceived) {
+      setShouldGetTxDetails(false);
+      // Process current signature (signatures[stats.index].signature)
+      // if its corresponding detail is not loaded into the transactions array
+      const currentSignature = signatures[stats.index];
+      // console.log('currentSignature:', currentSignature);
+      const needFetching = signatures.length > 0 &&
+                           (!transactions || transactions.length === 0 ||
+                            !transactions.some(tx => tx.signature === currentSignature.signature));
+      // console.log('needFetching:', needFetching);
+      // console.log('stats.index:', stats.index);
+      // console.log('signatures.length:', signatures.length);
+
+      // If no need to fetch the Tx detail and the signature is the last one in the list
+      if (!needFetching && stats.index >= (signatures.length - 1)) {
+        // Set the state to stop and finish the whole process
+        setLoadingTransactions(false);
+        // TODO: update stats
+        return;
+      }
+
+      if (needFetching) {
+        customConnection.getConfirmedTransaction(currentSignature.signature)
+          .then(confirmedTx => {
+            if (confirmedTx) {
+              const transWithSignature = new TransactionWithSignature(
+                currentSignature.signature,
+                confirmedTx
+              );
+              setTransactions(items => [...items, transWithSignature]);
+              // Increment index to select next signature
+              dispatch(new IncrementTransactionIndexAction());
+              setShouldGetTxDetails(true);
+              // TODO: update stats
+            }
+          })
+      } else {
+        // Increment index to select next signature
+        dispatch(new IncrementTransactionIndexAction());
+        // Set state to load next Tx details
+        setShouldGetTxDetails(true);
+        // TODO: update stats
+      }
+    }
+  }, [
+    stats,
+    publicKey,
+    signatures,
+    transactions,
+    customConnection,
+    shouldGetTxDetails,
+    abortSignalReceived,
+  ]);
+
+  // Keep stats in sync when transaction's list changes
+  useEffect(() => {
+    if (publicKey && transactions) {
+      const incoming = transactions.filter(tx => tx.confirmedTransaction.transaction.instructions[0].keys[1].pubkey.toBase58() === publicKey.toBase58());
+      const outgoing = transactions.filter(tx => tx.confirmedTransaction.transaction.instructions[0].keys[0].pubkey.toBase58() === publicKey.toBase58());
+      const newStats = Object.assign({}, stats, {
+        incoming: incoming.length,
+        outgoing: outgoing.length
+      });
+      dispatch(new SetStatsAction(newStats));
+    }
+  }, [
+    publicKey,
+    transactions
   ]);
 
   const renderTransactions = () => {
@@ -139,6 +225,7 @@ export const AccountsView = () => {
           )}
           </p>
           <p>Abort signal received: {abortSignalReceived ? 'true' : 'false'}</p>
+          <p>Tx: {stats.total ? stats.index + 1 : 0} of {stats.total} | incoming: {stats.incoming} outgoing: {stats.outgoing}</p>
           <div>{renderTransactions()}</div>
         </div>
       </div>
