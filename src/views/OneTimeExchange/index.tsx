9a5bb809a7c56c4ca9019879ae9015926ebebb40
Yamel Amador Fernandez <yamel_amador_fdez@msn.com>
2021-11-11 10:58:24 -0600

    Switch to using the standard ExchangeInput component for the destination token (ExchangeOutput component is commented out)

--- src/views/OneTimeExchange/index.tsx
+++ src/views/OneTimeExchange/index.tsx
@@ -2030,7 +2030,37 @@ export const OneTimeExchange = (props: {
           </div>
 
           {/* Destination token / amount */}
-          <ExchangeOutput
+          <ExchangeInput
+            token={toMint && mintList[toMint]}
+            tokenBalance={
+              (toMint && toBalance && mintList[toMint] && parseFloat(toBalance)
+                ? parseFloat(toBalance).toFixed(mintList[toMint].decimals)
+                : '')
+            }
+            tokenAmount={
+              (toMint && mintList[toMint] && exchangeInfo && exchangeInfo.amountIn && exchangeInfo.amountOut 
+                ? exchangeInfo.amountOut.toFixed(mintList[toMint].decimals)
+                : '')
+            }
+            onInputChange={() => {}}
+            onMaxAmount={() => {}}
+            onSelectToken={() => {
+              setSubjectTokenSelection("destination");
+              showTokenSelector();
+            }}
+            inputPosition={inputPosition}
+            translationId="destination"
+            inputLabel={
+              toMint && mintList[toMint]
+                ? `~$${
+                  exchangeInfo && exchangeInfo.amountIn && exchangeInfo.amountOut
+                  ? formatAmount(parseFloat(exchangeInfo.amountOut.toFixed(mintList[toMint].decimals)) * getPricePerToken(mintList[toMint] as TokenInfo), 2)
+                  : '0.00'}`
+                : ''
+            }
+          />
+
+          {/* <ExchangeOutput
             fromToken={fromMint && mintList[fromMint]}
             fromTokenAmount={fromAmount}
             toToken={toMint && mintList[toMint]}
@@ -2061,7 +2091,7 @@ export const OneTimeExchange = (props: {
               consoleOut('onSelectedClient:', client, 'blue');
               setSelectedClient(client);
             }}
-          />
+          /> */}
 
           {/* Title bar with settings */}
           <div className="info-line-and-settings flexible-left">
