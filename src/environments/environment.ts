8c20ee54acbf072b6ae85234e5200f32f8278b9a
Yamel Amador Fernandez <yamel_amador_fdez@msn.com>
2021-12-21 08:48:16 -0600

    New IDO address

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -59,7 +59,7 @@ export class AppConfigService {
             influxDbBucket: 'meanfi-dev',
             logglyCustomerKey: '8aaea666-b5e8-469b-828a-89c9ca60cdef',
             logglyTag: 'MeanFiWebApp',
-            idoAccountAddress: 'CZRP2Byd5YPDa2PShNK3rtHzAmkHawjQK3AbCYa3UkQ1'
+            idoAccountAddress: 'DgHgjuTFc4pDtA21QA1wgzcZMF5A8Mg32QqkpkN2VBc2'
         },
         local: {
             appUrl: 'http://localhost:3000',
@@ -73,7 +73,7 @@ export class AppConfigService {
             influxDbBucket: 'meanfi-dev',
             logglyCustomerKey: '8aaea666-b5e8-469b-828a-89c9ca60cdef',
             logglyTag: 'MeanFiWebApp',
-            idoAccountAddress: 'CZRP2Byd5YPDa2PShNK3rtHzAmkHawjQK3AbCYa3UkQ1'
+            idoAccountAddress: 'DgHgjuTFc4pDtA21QA1wgzcZMF5A8Mg32QqkpkN2VBc2'
         },
     };
 
