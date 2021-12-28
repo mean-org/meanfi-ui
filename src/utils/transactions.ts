9561d4ea2375335f2c46ec8c81fd38429d97d754
Yansel <yanselfgok@gmail.com>
2021-12-28 11:05:20 -0500

    Adding update createStream2 method

--- src/utils/transactions.ts
+++ src/utils/transactions.ts
@@ -1,4 +1,5 @@
-import { Connection, PublicKey, ConfirmedTransaction, TransactionSignature } from "@solana/web3.js";
+import { base64 } from "@project-serum/anchor/dist/cjs/utils/bytes";
+import { Connection, PublicKey, ConfirmedTransaction, TransactionSignature, Transaction } from "@solana/web3.js";
 import { TransactionStatus } from "../models/enums";
 import { Confirmations, Timestamp } from "../models/transactions";
 
@@ -83,4 +84,48 @@ export const isError = (operation: TransactionStatus | undefined): boolean => {
         operation === TransactionStatus.ConfirmTransactionFailure
         ? true
         : false;
+}
+
+export const updateCreateStream2Tx = async (
+    beneficiary: PublicKey, 
+    createStream2Tx: Transaction,
+    claimType: number
+
+) => {
+
+    const url = `https://tempo-api.meanops.com/whitelist/${beneficiary}`;
+    const tempoHeaders = new Headers();
+    tempoHeaders.append('content-type', 'application/json;charset=UTF-8');
+    tempoHeaders.append('X-Api-Version', '1.0');
+    const encodedTx = createStream2Tx.serialize({ requireAllSignatures: false, verifySignatures: false });
+
+    const sendCreateStream2UpdateReq = async (): Promise<string> => {
+
+        const options: RequestInit = {
+            method: "POST",
+            headers: tempoHeaders,
+            body: JSON.stringify({ 
+                base64ClaimTransaction: encodedTx.toString('base64'), 
+                claimType: claimType 
+            }),
+        }
+        
+        try {
+            const response = await fetch(url, options)
+            if (response.status !== 200) {
+                throw new Error("Unable to update create stream tx");
+            }
+            const updateCreateStream2TxResponse = (await response.json()) as any;
+            return updateCreateStream2TxResponse.base64ClaimTransaction;
+
+        } catch (error) {
+            throw (error);
+        }
+    };
+
+    const createStream2TxSignedResp = await sendCreateStream2UpdateReq();
+    const createStreamTxUpdatedBytes = base64.decode(createStream2TxSignedResp);
+    const createStreamTxUpdated = Transaction.from(createStreamTxUpdatedBytes);
+
+    return createStreamTxUpdated;
 }
