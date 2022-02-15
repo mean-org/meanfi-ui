e00826d740301fd19aa3d0bef7c76a6e11f70de5
Tania Ballester <tballester.email@gmail.com>
2022-02-15 22:12:18 +0100

    Include the two panel layout to "Invest" page

--- src/pages/invest/index.tsx
+++ src/pages/invest/index.tsx
@@ -1,7 +1,13 @@
+import { ReloadOutlined } from "@ant-design/icons";
+import { Button, Tooltip } from "antd";
 import { PreFooter } from "../../components/PreFooter";
 import { IconStats } from "../../Icons";
+import { useTranslation } from 'react-i18next';
+import './style.less';
 
 export const InvestView = () => {
+  const { t } = useTranslation('common');
+
   return (
     <>
       <div className="container main-container">
@@ -9,10 +15,42 @@ export const InvestView = () => {
           <div className="title-and-subtitle">
             <div className="title">
               <IconStats className="mean-svg-icons" />
-              <div>Invest</div>
+              <div>{t('invest.title')}</div>
             </div>
             <div className="subtitle">
-              The easiest way to grow your money stash
+            {t('invest.subtitle')}
+            </div>
+          </div>
+          <div className="meanfi-two-panel-layout">
+            <div className="meanfi-two-panel-left">
+              <div className="meanfi-panel-heading">
+                <span className="title">{t('invest.screen-title')}</span>
+                <Tooltip placement="bottom" title={t('invest.refresh-tooltip')}>
+                  <div className="transaction-stats user-address">
+                    <span className="incoming-transactions-amout">(7)</span>
+                    <span className="transaction-legend">
+                      <span className="icon-button-container">
+                        <Button
+                          type="default"
+                          shape="circle"
+                          size="small"
+                          icon={<ReloadOutlined />}
+                          onClick={() => {}}
+                        />
+                      </span>
+                    </span>
+                  </div>
+                </Tooltip>
+              </div>
+              <div className="inner-container">
+
+              </div>
+            </div>
+
+            <div className="meanfi-two-panel-right">
+              <div className="inner-container">
+
+              </div>
             </div>
           </div>
         </div>
@@ -20,4 +58,4 @@ export const InvestView = () => {
       <PreFooter />
     </>
   );
-}
+};
