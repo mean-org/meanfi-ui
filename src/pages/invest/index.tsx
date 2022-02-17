84521cf1add9b2d1212b3a5d11ea9dd5893217f8
Tania Ballester <tballester.email@gmail.com>
2022-02-17 15:18:11 +0100

    First print of the tabset for staking

--- src/pages/invest/index.tsx
+++ src/pages/invest/index.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
+import './style.less';
 import { ReloadOutlined } from "@ant-design/icons";
 import { Button, Tooltip, Row, Col } from "antd";
 import { PreFooter } from "../../components/PreFooter";
 import { IconStats } from "../../Icons";
 import { useTranslation } from 'react-i18next';
-import './style.less';
+import Checkbox from "antd/lib/checkbox/Checkbox";
+
+type SwapOption = "stake" | "unstake";
 
 export const InvestView = () => {
   const { t } = useTranslation('common');
+  const [currentTab, setCurrentTab] = useState<SwapOption>("stake");
+
+  const onTabChange = (option: SwapOption) => {
+    setCurrentTab(option);
+  }
 
   const renderInvestOptions = (
     <div className="transaction-list-row money-streams-summary">
@@ -38,7 +47,7 @@ export const InvestView = () => {
             {t('invest.subtitle')}
             </div>
           </div>
-          <div className="meanfi-two-panel-layout">
+          <div className="meanfi-two-panel-layout invest-layout">
             <div className="meanfi-two-panel-left">
               <div className="meanfi-panel-heading">
                 <span className="title">{t('invest.screen-title')}</span>
@@ -95,6 +104,95 @@ export const InvestView = () => {
                     </Row>
                   </div>
                 </div>
+                <Row>
+                  <Col span={12}>
+                    <div className="place-transaction-box mb-3">
+                      <div className="button-tabset-container">
+                        <div className={`tab-button ${currentTab === "stake" ? 'active' : ''}`} onClick={() => onTabChange("stake")}>
+                          {t('invest.tabset.stake')}
+                        </div>
+                        <div className={`tab-button ${currentTab === "unstake" ? 'active' : ''}`} onClick={() => onTabChange("unstake")}>
+                          {t('invest.tabset.unstake')}
+                        </div>
+                      </div>
+                      {/* STAKE */}
+                      {
+                        currentTab === "stake" && (
+                          <>
+                          <div className="form-label">Amount to stake</div>
+                          <div className="well">
+                            <div className="flex-fixed-left">
+                              <div className="left">
+                                <span className="add-on simplelink">
+                                </span>
+                              </div>
+                              <div className="right">
+                                <input
+                                  className="general-text-input text-right"
+                                  inputMode="decimal"
+                                  autoComplete="off"
+                                  autoCorrect="off"
+                                  type="text"
+                                  pattern="^[0-9]*[.,]?[0-9]*$"
+                                  placeholder="0.0"
+                                  minLength={1}
+                                  maxLength={79}
+                                  spellCheck="false"
+                                />
+                              </div>
+                            </div>
+                            <div className="flex-fixed-right">
+                              <div className="left inner-label">
+
+                              </div>
+                              <div className="right inner-label">
+
+                              </div>
+                            </div>
+                          </div>
+                          <span className="info-label">How long do you want to stake for?</span>
+                          <div className="flexible-left mb-1 mt-2">
+                            <div className="left token-group">
+                              <div className="token-max simplelink mb-1">7 days
+                              </div> 
+                              <div className="token-max simplelink mb-1">30 days</div> 
+                              <div className="token-max simplelink mb-1">90 days</div> 
+                              <div className="token-max simplelink mb-1">1 year</div> 
+                              <div className="token-max simplelink mb-1">4 years</div> 
+                            </div>
+                          </div>
+                          <span className="info-label">Staking period of 7 days will lock your Tokens until February 21, 2022 with estimated rewards of up to 100 MEAN</span>
+
+                          {/* Confirm that have read the terms and conditions */}
+                          <div className="mb-2 mt-2">
+                            <Checkbox>{"I agree to the Terms & Conditions"}</Checkbox>
+                          </div>
+
+                            {/* Action button */}
+                            <Button
+                              className="main-cta"
+                              block
+                              type="primary"
+                              shape="round"
+                              size="large"
+                            >
+                              Stake MEAN
+                            </Button>
+                          </>
+                        )
+                      }
+                      {/* UNSTAKE */}
+                      {
+                        currentTab === "unstake" && (
+                          <div>Unstake</div>
+                        )
+                      }
+                    </div>
+                  </Col>
+                  <Col span={12}>
+                    
+                  </Col>
+                </Row>
               </div>
             </div>
           </div>
