c7015db8219363794f3bf2e295d558b2243c6b6c
Tania Ballester <tballester.email@gmail.com>
2022-05-16 08:43:30 -0400

    Include a loading text when the txs are loading

--- src/pages/safe/components/ProgramDetails/index.tsx
+++ src/pages/safe/components/ProgramDetails/index.tsx
@@ -61,6 +61,7 @@ export const ProgramDetailsView = (props: {
   const [/*ongoingOperation*/, setOngoingOperation] = useState<OperationType | undefined>(undefined);
   const [/*retryOperationPayload*/, setRetryOperationPayload] = useState<any>(undefined);
   const [selectedProgram, setSelectedProgram] = useState<ProgramAccounts | undefined>(undefined);
+  const [loadingTxs, setLoadingTxs] = useState(true);
 
   // When back button is clicked, goes to Safe Info
   const hideProgramDetailsHandler = () => {
@@ -873,6 +874,7 @@ export const ProgramDetailsView = (props: {
         .catch(error => {
           console.error(error);
         })
+        .finally(() => setLoadingTxs(false));
   }, [connection, programSignatures]);
 
   const renderTransactions = (
@@ -884,31 +886,37 @@ export const ProgramDetailsView = (props: {
           <Col span={5} className="std-table-cell pl-3 pr-1">Time</Col>
         </Row>
       </div>
-      {programTransactions && (
-        programTransactions.map((tx: any) => (
-          <Row gutter={[8, 8]} className="item-list-body compact hover-list w-100 pt-1" key={tx.blockTime}>
-            <Col span={14} className="std-table-cell pr-1 simplelink signature">
-              <CopyExtLinkGroup 
-                content={tx.transaction.signatures.slice(0, 1).shift()}
-                externalLink={true}
-                className="text-truncate"
-                message="Signature"
-                isTx={true}
-              />
-            </Col>
-            <Col span={5} className="std-table-cell pr-1 simplelink">
-              <CopyExtLinkGroup 
-                content={formatThousands(tx.slot)}
-                externalLink={false}
-                className="text-truncate"
-                message="Slot"
-              />
-            </Col>
-            <Col span={5} className="std-table-cell pr-1">
-              {moment.unix(tx.blockTime).fromNow()}
-            </Col>
-          </Row>
-        ))
+      {!loadingTxs ? (
+        (programTransactions && programTransactions.length > 0) ? (
+          programTransactions.map((tx: any) => (
+            <Row gutter={[8, 8]} className="item-list-body compact hover-list w-100 pt-1" key={tx.blockTime}>
+              <Col span={14} className="std-table-cell pr-1 simplelink signature">
+                <CopyExtLinkGroup 
+                  content={tx.transaction.signatures.slice(0, 1).shift()}
+                  externalLink={true}
+                  className="text-truncate"
+                  message="Signature"
+                  isTx={true}
+                />
+              </Col>
+              <Col span={5} className="std-table-cell pr-1 simplelink">
+                <CopyExtLinkGroup 
+                  content={formatThousands(tx.slot)}
+                  externalLink={false}
+                  className="text-truncate"
+                  message="Slot"
+                />
+              </Col>
+              <Col span={5} className="std-table-cell pr-1">
+                {moment.unix(tx.blockTime).fromNow()}
+              </Col>
+            </Row>
+          ))
+        ) : (
+          <span>This program has no transactions</span>
+        )
+      ) : (
+        <span>Loading...</span>
       )}
     </>
   )
