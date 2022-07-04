e7900ec6829736081ae7cbeba7fa49474fc837f3
Tania Ballester <tballester.email@gmail.com>
2022-07-04 14:42:25 -0400

    Change tooltip text in account addres

--- src/pages/accounts/index.tsx
+++ src/pages/accounts/index.tsx
@@ -4923,7 +4923,7 @@ export const AccountsNewView = () => {
                             </span>
                           )}
                           <span className="icon-button-container">
-                            <Tooltip placement="bottom" title={t('assets.account-address-copy-cta')}>
+                            <Tooltip placement="top" title={t('assets.account-address-copy-cta')}>
                               <Button
                                 type="default"
                                 shape="circle"
