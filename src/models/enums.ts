21322341710904e78bd7f2c243147ba26b21fcc9
Yamel Amador Fernandez <yamel_amador_fdez@msn.com>
2022-07-13 08:40:18 -0500

    Update Multisig SDK version to 1.16.0-alpha.1 | Remove duplicate enum definition

--- src/models/enums.ts
+++ src/models/enums.ts
@@ -204,26 +204,3 @@ export const getCategoryLabelByValue = (value: SubCategory) => {
     }
     return '-';
 }
-
-export enum MetaInfoCtaAction {
-    Send = 0,
-    Buy = 1,
-    Exchange = 2,
-    Invest = 3,
-    Deposit = 4,
-    UnwrapSol = 5,
-    WrapSol = 6,
-    MergeAccounts = 7,
-    Divider = 10,
-    Refresh = 11,
-    CloseAccount = 12,
-    Share = 13,
-    Close = 14,
-    VestingContractCreateStreamOnce = 20,
-    VestingContractCreateStreamBulk = 21,
-    VestingContractAddFunds = 21,
-    VestingContractViewSolBalance = 22,
-    VestingContractWithdrawFunds = 23,
-    VestingContractRefreshAccount = 24,
-    VestingContractClose = 25,
-}
