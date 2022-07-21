9713efa8bc750db00bfc6c4c67e3e342663fa349
Tania Ballester <tballester.email@gmail.com>
2022-07-21 11:21:36 -0400

    Change the position of the btns in MS info panel

--- src/views/MoneyStreamsInfo/index.tsx
+++ src/views/MoneyStreamsInfo/index.tsx
@@ -2645,9 +2645,15 @@ export const MoneyStreamsInfoView = (props: {
   // Dropdown (three dots button) inside outgoing stream list
   const menu = (
     <Menu>
-      <Menu.Item key="00" onClick={param === "multisig" ? showCreateStreamModal : showCreateMoneyStreamModal}>
-        <span className="menu-item-text">Create stream</span>
-      </Menu.Item>
+      {param === "multisig" ? (
+        <Menu.Item key="00" onClick={showAddFundsModal}>
+          <span className="menu-item-text">Fund account</span>
+        </Menu.Item>
+      ) : (
+        <Menu.Item key="00" onClick={showOpenStreamModal}>
+          <span className="menu-item-text">Find stream</span>
+        </Menu.Item>
+      )}
     </Menu>
   );
 
@@ -2658,7 +2664,7 @@ export const MoneyStreamsInfoView = (props: {
           infoData={infoData}
         />
 
-        <Row gutter={[8, 8]} className="safe-btns-container mb-1">
+        <Row gutter={[8, 8]} className="safe-btns-container d-flex align-items-center mb-1">
           <Col xs={isXsDevice ? 20 : 24} sm={isXsDevice ? 18 : 24} md={isXsDevice ? 20 : 24} lg={isXsDevice ? 18 : 24} className="btn-group">
             <Button
               type="default"
@@ -2671,45 +2677,45 @@ export const MoneyStreamsInfoView = (props: {
                   {/* {param === "multisig" ? "Initiate streaming account" : "Create streaming account"} */}
                 </div>
             </Button>
-            {param === "multisig" ? (
-              <Button
-                type="default"
-                shape="round"
-                size="small"
-                className="thin-stroke btn-min-width"
-                onClick={showAddFundsModal}>
-                  <div className="btn-content">
-                    Fund account
-                  </div>
-              </Button>
-            ) : (
-              <Button
-                type="default"
-                shape="round"
-                size="small"
-                className="thin-stroke btn-min-width"
-                onClick={showOpenStreamModal}>
-                  <div className="btn-content">
-                    Find stream
-                  </div>
-              </Button>
-            )}
+            <Button
+              type="default"
+              shape="round"
+              size="small"
+              className="thin-stroke btn-min-width"
+              onClick={() => {
+                param === "multisig"
+                  ? showCreateStreamModal()
+                  : showCreateMoneyStreamModal()
+              }}>
+              <div className="btn-content">
+                {/* {param === "multisig" ? "Initiate stream" : "Create stream"} */}
+                Create stream
+              </div>
+            </Button>
             {!isXsDevice && (
-              <Button
-                type="default"
-                shape="round"
-                size="small"
-                className="thin-stroke btn-min-width"
-                onClick={() => {
-                  param === "multisig"
-                    ? showCreateStreamModal()
-                    : showCreateMoneyStreamModal()
-                }}>
-                <div className="btn-content">
-                  {/* {param === "multisig" ? "Initiate stream" : "Create stream"} */}
-                  Create stream
-                </div>
-              </Button>
+              param === "multisig" ? (
+                <Button
+                  type="default"
+                  shape="round"
+                  size="small"
+                  className="thin-stroke btn-min-width"
+                  onClick={showAddFundsModal}>
+                    <div className="btn-content">
+                      Fund account
+                    </div>
+                </Button>
+              ) : (
+                <Button
+                  type="default"
+                  shape="round"
+                  size="small"
+                  className="thin-stroke btn-min-width"
+                  onClick={showOpenStreamModal}>
+                    <div className="btn-content">
+                      Find stream
+                    </div>
+                </Button>
+              )
             )}
           </Col>
 
