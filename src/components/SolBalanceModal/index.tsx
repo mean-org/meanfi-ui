5167ae633861c66b42cc0e41c2a5675b3b0fbc5d
Yamel Amador Fernandez <yamel_amador_fdez@msn.com>
2022-08-03 09:38:53 -0500

    Font size, color and weight corrections on SOL balance modal

--- src/components/SolBalanceModal/index.tsx
+++ src/components/SolBalanceModal/index.tsx
@@ -35,7 +35,7 @@ export const SolBalanceModal = (props: {
 
   return (
     <Modal
-      className="mean-modal simple-modal"
+      className="mean-modal simple-modal unpadded-content"
       title={<div className="modal-title">SOL Balance</div>}
       footer={null}
       visible={isVisible}
@@ -44,63 +44,62 @@ export const SolBalanceModal = (props: {
       width={360}>
       <div className="buy-token-options">
         <div className="text-center">
-          <div className="d-flex flex-column mb-1">
-            {/* Balance title */}
+          {/* Balance title */}
+          {isStreamingAccount ? (
+            <p className="mb-0">
+              Your SOL streaming account balance:
+            </p>
+          ) : (
+            <p className="mb-0">
+              Balance of SOL in safe:
+            </p>
+          )}
+          {/* Balance value */}
+          <p className="mb-2">
             {isStreamingAccount ? (
-              <span className="info-label">
-                Your SOL streaming account balance:
-              </span>
-            ) : (
-              <span className="info-label">
-                Balance of SOL in safe:
-              </span>
-            )}
-            {/* Balance value */}
-            <span className="info-value">
-              {isStreamingAccount ? (
-                <>
-                  {treasuryBalance !== undefined ? (
-                    <>
-                      {getTokenAmountAndSymbolByTokenAddress(
-                        treasuryBalance,
-                        NATIVE_SOL_MINT.toBase58()
-                      )}
-                    </>
-                  ) : (
-                    <IconLoading className="mean-svg-icons" style={{ height: "12px", lineHeight: "12px" }} />
-                  )}
-                </>
-              ) : (
-                <>
-                  {selectedMultisig && selectedMultisig !== undefined ? (
-                    <>
-                      {getTokenAmountAndSymbolByTokenAddress(
-                        toUiAmount(new BN(selectedMultisig.balance), NATIVE_SOL.decimals || 9),
-                        NATIVE_SOL_MINT.toBase58()
-                      )}
-                    </>
-                  ) : (
-                    <IconLoading className="mean-svg-icons" style={{ height: "12px", lineHeight: "12px" }} />
-                  )}
-                </>
-              )}
-            </span>
-            {/* Warn if Safe balance is low */}
-            {isStreamingAccount && (
               <>
-                {(selectedMultisig && (toUiAmount(new BN(selectedMultisig.balance), NATIVE_SOL.decimals) < MIN_SOL_BALANCE_REQUIRED)) ? (
-                  <span className="form-field-error">
-                    You are running low on SOL needed <br />
-                    to pay for transaction fees.
-                  </span>
-                ) : null}
+                {treasuryBalance !== undefined ? (
+                  <>
+                    {getTokenAmountAndSymbolByTokenAddress(
+                      treasuryBalance,
+                      NATIVE_SOL_MINT.toBase58()
+                    )}
+                  </>
+                ) : (
+                  <IconLoading className="mean-svg-icons" style={{ height: "12px", lineHeight: "12px" }} />
+                )}
+              </>
+            ) : (
+              <>
+                {selectedMultisig && selectedMultisig !== undefined ? (
+                  <>
+                    {getTokenAmountAndSymbolByTokenAddress(
+                      toUiAmount(new BN(selectedMultisig.balance), NATIVE_SOL.decimals || 9),
+                      NATIVE_SOL_MINT.toBase58()
+                    )}
+                  </>
+                ) : (
+                  <IconLoading className="mean-svg-icons" style={{ height: "12px", lineHeight: "12px" }} />
+                )}
               </>
             )}
-          </div>
+          </p>
+          {/* Warn if Safe balance is low */}
+          {isStreamingAccount && (
+            <p>
+              {(selectedMultisig && (toUiAmount(new BN(selectedMultisig.balance), NATIVE_SOL.decimals) < MIN_SOL_BALANCE_REQUIRED)) ? (
+                <span className="form-field-error">
+                  You are running low on SOL needed <br />
+                  to pay for transaction fees.
+                </span>
+              ) : null}
+            </p>
+          )}
+
           {isStreamingAccount ? (
-            <h4 className="mb-3">Scan the QR code to send SOL to this account</h4>
+            <p>Scan the QR code to send SOL to this account</p>
           ) : (
-            <h4 className="mb-3">Scan the QR code to send SOL to this safe</h4>
+            <p>Scan the QR code to send SOL to this safe</p>
           )}
 
           <div className="qr-container bg-white">
@@ -110,19 +109,19 @@ export const SolBalanceModal = (props: {
             />
           </div>
 
-          <div className="flex-center font-size-70 mb-2">
+          <div className="flex-center mb-1">
             <AddressDisplay
               address={isStreamingAccount ? address : multisigAddress as string}
-              showFullAddress={true}
-              iconStyles={{ width: "15", height: "15" }}
+              maxChars={12}
+              iconStyles={{ width: "16", height: "16", verticalAlign: "-2" }}
               newTabLink={`${SOLANA_EXPLORER_URI_INSPECT_ADDRESS}${isStreamingAccount ? address : multisigAddress}${getSolanaExplorerClusterParam()}`}
             />
           </div>
 
           {!isStreamingAccount ? (
-            <div className="font-light font-size-75 px-4">This address can only be used to receive SOL for this safe</div>
+            <p className="px-5">This address can only be used to receive SOL for this safe</p>
           ) : (
-            <div className="font-light font-size-75 px-4">This address can only be used to receive SOL to pay for the transaction fees for this streaming account</div>
+            <p className="px-5">This address can only be used to receive SOL to pay for the transaction fees for this streaming account</p>
           )}
         </div>
       </div>
