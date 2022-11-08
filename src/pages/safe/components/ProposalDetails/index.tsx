905fc04eac18a22368eae862e3bb02fcf9ceb4dc
Yamel Amador Fernandez <yamel.amador@realdax.com>
2022-11-08 10:40:41 -0600

    Code-splitting refactor to separate the list of instruction fields to a new component

--- src/pages/safe/components/ProposalDetails/index.tsx
+++ src/pages/safe/components/ProposalDetails/index.tsx
@@ -2,33 +2,43 @@ import { App, AppConfig } from '@mean-dao/mean-multisig-apps';
 import { MeanMultisig, MultisigParticipant, MultisigTransaction, MultisigTransactionActivityItem, MultisigTransactionStatus } from '@mean-dao/mean-multisig-sdk';
 import { IDL as SplTokenIdl } from '@project-serum/anchor/dist/cjs/spl/token';
 import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
-import { PublicKey, SystemProgram } from '@solana/web3.js';
-import { Button, Col, Row, Tooltip } from "antd";
-import BN from 'bn.js';
+import { Connection, PublicKey, SystemProgram } from '@solana/web3.js';
+import { Button, Col, Row } from "antd";
 import { openNotification } from 'components/Notifications';
 import { RejectCancelModal } from 'components/RejectCancelModal';
 import { ResumeItem } from 'components/ResumeItem';
 import { TabsMean } from 'components/TabsMean';
-import { SOLANA_EXPLORER_URI_INSPECT_ADDRESS, SOLANA_EXPLORER_URI_INSPECT_TRANSACTION } from 'constants/common';
+import { SOLANA_EXPLORER_URI_INSPECT_TRANSACTION } from 'constants/common';
 import { AppStateContext } from 'contexts/appstate';
 import { getSolanaExplorerClusterParam } from 'contexts/connection';
 import { TxConfirmationContext } from 'contexts/transaction-status';
 import { useWallet } from 'contexts/wallet';
-import { IconApprove, IconArrowBack, IconCreated, IconCross, IconExternalLink, IconLightning, IconMinus, IconThumbsDown, IconThumbsUp, IconUser, IconUserClock } from "Icons";
-import { appConfig } from 'index';
-import { consoleOut, copyText, getDurationUnitFromSeconds } from 'middleware/ui';
-import { displayAmountWithSymbol, formatThousands, getTokenOrCustomToken, makeDecimal, shortenAddress } from 'middleware/utils';
+import {
+  IconApprove,
+  IconArrowBack,
+  IconCreated,
+  IconCross,
+  IconExternalLink,
+  IconLightning,
+  IconMinus,
+  IconThumbsDown,
+  IconThumbsUp,
+  IconUser,
+  IconUserClock
+} from "Icons";
+import { consoleOut, copyText } from 'middleware/ui';
+import { shortenAddress } from 'middleware/utils';
 import { OperationType, TransactionStatus } from 'models/enums';
-import { createAnchorProgram, InstructionAccountInfo, InstructionDataInfo, MultisigTransactionInstructionInfo, parseMultisigProposalIx, parseMultisigSystemProposalIx } from 'models/multisig';
-import { TokenInfo } from 'models/SolanaTokenInfo';
+import { createAnchorProgram, MultisigTransactionInstructionInfo, parseMultisigProposalIx, parseMultisigSystemProposalIx } from 'models/multisig';
 import moment from "moment";
-import { useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { RenderInstructions } from './RenderInstructions';
 import './style.scss';
 
 export const ProposalDetailsView = (props: {
   appsProvider?: any;
-  connection?: any;
+  connection: Connection;
   hasMultisigPendingProposal?: boolean;
   isBusy: boolean;
   loadingData: boolean;
@@ -44,9 +54,7 @@ export const ProposalDetailsView = (props: {
 }) => {
 
   const {
-    splTokenList,
     transactionStatus,
-    getTokenByMintAddress,
     setTransactionStatus,
   } = useContext(AppStateContext);
   const { t } = useTranslation('common');
@@ -75,9 +83,6 @@ export const ProposalDetailsView = (props: {
   const [needReloadActivity, setNeedReloadActivity] = useState<boolean>(false);
   const [loadingActivity, setLoadingActivity] = useState<boolean>(false);
   const [isCancelRejectModalVisible, setIsCancelRejectModalVisible] = useState(false);
-  const [proposalIxAssociatedToken, setProposalIxAssociatedToken] = useState<TokenInfo | undefined>(undefined);
-
-  const multisigAddressPK = useMemo(() => new PublicKey(appConfig.getConfig().multisigProgramAddress), []);
 
   const resetTransactionStatus = useCallback(() => {
     setTransactionStatus({
@@ -265,27 +270,6 @@ export const ProposalDetailsView = (props: {
     needReloadActivity,
   ])
 
-  useEffect(() => {
-    if (!proposalIxInfo || !proposalIxInfo.accounts || proposalIxInfo.accounts.length === 0) {
-      return undefined;
-    }
-    const idx = proposalIxInfo.accounts.findIndex(p => p.label === 'Associated Token');
-    if (idx !== -1) {
-      const associatedToken = proposalIxInfo.accounts[idx].value;
-      getTokenOrCustomToken(connection, associatedToken, getTokenByMintAddress)
-      .then(token => {
-        consoleOut('proposal Associated Token ->', token, 'blue');
-        setProposalIxAssociatedToken(token);
-      });
-    } else {
-      setProposalIxAssociatedToken(undefined);
-    }
-  }, [connection, getTokenByMintAddress, proposalIxInfo]);
-
-  // const isUnderDevelopment = () => {
-  //   return isLocal() || (isDev() && isWhitelisted) ? true : false;
-  // }
-
   // When back button is clicked, goes to Safe Info
   const hideProposalDetailsHandler = () => {
     // Sends the value to the parent component "SafeView"
@@ -308,160 +292,6 @@ export const ProposalDetailsView = (props: {
     }
 
   },[t]);
-
-  const getTokenAmountValue = (item: InstructionDataInfo) => {
-    switch (item.label) {
-      case 'CliffVestPercent':
-        return `${makeDecimal(new BN(item.value), 4)}%`;
-      case 'RateIntervalInSeconds':
-        return `${formatThousands(item.value)}s (${getDurationUnitFromSeconds(+item.value)})`;
-      case 'StartUtc':
-        return moment(+item.value).format("LLL").toLocaleString();
-      case 'Amount':
-      case 'RateAmountUnits':
-      case 'AllocationAssignedUnits':
-        return proposalIxAssociatedToken
-          ? displayAmountWithSymbol(
-              item.value,
-              proposalIxAssociatedToken.address,
-              proposalIxAssociatedToken.decimals,
-              splTokenList,
-            )
-          : item.value;
-      default:
-        return item.value;
-    }
-  }
-
-  // Display the instructions in the "Instructions" tab, on safe details page
-  const renderInstructions = (
-    proposalIxInfo ? (
-      <div className="safe-details-collapse w-100 pl-1 pr-4">
-        <Row gutter={[8, 8]} className="mb-2 mt-2" key="programs">
-          <Col xs={6} sm={6} md={4} lg={4} className="pr-1">
-            <span className="info-label">{t('multisig.proposal-modal.instruction-program')}</span>
-          </Col>
-          <Col xs={17} sm={17} md={19} lg={19} className="pl-1 pr-3">
-            <span onClick={() => copyAddressToClipboard(proposalIxInfo.programId)}  className="d-block info-data simplelink underline-on-hover text-truncate" style={{cursor: 'pointer'}}>
-              {(
-                proposalIxInfo.programName 
-                  ? `${proposalIxInfo.programName} (${proposalIxInfo.programId})` 
-                  : proposalIxInfo.programId
-              )}
-            </span>
-          </Col>
-          <Col xs={1} sm={1} md={1} lg={1}>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={`${SOLANA_EXPLORER_URI_INSPECT_ADDRESS}${proposalIxInfo.programId}${getSolanaExplorerClusterParam()}`}>
-              <IconExternalLink className="mean-svg-icons external-icon" />
-            </a>
-          </Col>
-        </Row>
-        
-        {
-          proposalIxInfo.accounts.map((account: InstructionAccountInfo, index: number) => {
-            return (
-              <Row gutter={[8, 8]} className="mb-2" key={`item-${index}`}>
-                <Col xs={6} sm={6} md={4} lg={4} className="pr-1">
-                  <span className="info-label">{account.label || t('multisig.proposal-modal.instruction-account')}</span>
-                </Col>
-                <Col xs={17} sm={17} md={19} lg={19} className="pl-1 pr-3">
-                  <span onClick={() => copyAddressToClipboard(account.value)} className="d-block info-data simplelink underline-on-hover text-truncate" style={{cursor: 'pointer'}}>
-                    {account.value}
-                  </span>
-                </Col>
-                <Col xs={1} sm={1} md={1} lg={1}>
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href={`${SOLANA_EXPLORER_URI_INSPECT_ADDRESS}${account.value}${getSolanaExplorerClusterParam()}`}>
-                    <IconExternalLink className="mean-svg-icons external-icon" />
-                  </a>
-                </Col>
-              </Row>
-            )
-          })
-        }
-
-        {
-          proposalIxInfo.programId === multisigAddressPK.toBase58() ? (
-            proposalIxInfo.data.map((item: InstructionDataInfo, index: number) => {
-              return (
-                <Row gutter={[8, 8]} className="mb-2" key={`more-items-${index}`}>
-                  { item.label && (
-                    <Col xs={6} sm={6} md={4} lg={4} className="pr-1 text-truncate">
-                      <Tooltip placement="right" title={item.label || ""}>
-                        <span className="info-label">{item.label || t('multisig.proposal-modal.instruction-data')}</span>
-                      </Tooltip>
-                    </Col>
-                  )}
-                  {
-                    item.label === "Owners" ? (
-                      <>
-                        {item.value.map((owner: any, idx: number) => {
-                          return (
-                            <Row key={`owners-${idx}`} className="pr-1">
-                              <Col xs={6} sm={6} md={4} lg={4} className="pl-1 pr-1 text-truncate">
-                                <Tooltip placement="right" title={owner.label || ""}>
-                                  <span className="info-label">{owner.label || t('multisig.proposal-modal.instruction-data')}</span>
-                                </Tooltip>
-                              </Col>
-                              <Col xs={17} sm={17} md={19} lg={19} className="pl-1 pr-3">
-                                <span onClick={() => copyAddressToClipboard(owner.data)} className="d-block info-data simplelink underline-on-hover text-truncate" style={{cursor: 'pointer'}}>
-                                  {owner.data}
-                                </span>
-                              </Col>
-                              <Col xs={1} sm={1} md={1} lg={1}>
-                                <a
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                  href={`${SOLANA_EXPLORER_URI_INSPECT_ADDRESS}${owner.data}${getSolanaExplorerClusterParam()}`}>
-                                  <IconExternalLink className="mean-svg-icons external-icon" />
-                                </a>
-                              </Col>
-                            </Row>
-                          )
-                        })}
-                      </>
-                    ) : (
-                      <>
-                        <Col xs={17} sm={17} md={19} lg={19} className="pl-1 pr-3" key="loquejea">
-                          <span className="d-block info-data text-truncate" style={{cursor: 'pointer'}}>
-                            {item.value}
-                          </span>
-                        </Col>
-                      </>
-                    )
-                  }
-                </Row>
-              )
-            })
-          ) : (
-            proposalIxInfo.data.map((item: InstructionDataInfo, index: number) => {
-              return item.label && item.value && (
-                <Row gutter={[8, 8]} className="mb-2" key={`data-${index}`}>
-                  <Col xs={6} sm={6} md={4} lg={4} className="pr-1 text-truncate">
-                    <Tooltip placement="right" title={item.label || ""}>
-                      <span className="info-label">{item.label || t('multisig.proposal-modal.instruction-data')}</span>
-                    </Tooltip>
-                  </Col>
-                  <Col xs={17} sm={17} md={19} lg={19} className="pl-1 pr-3">
-                    <span className="d-block info-data text-truncate" style={{cursor: 'pointer'}}>
-                      {getTokenAmountValue(item)}
-                    </span>
-                  </Col>
-                </Row>
-              )
-            })
-          )
-        }
-      </div>
-    ) : (
-      <span className="pl-1">Loading instruction...</span>
-    )
-  );
 
   // Display the activities in the "Activity" tab, on safe details page
   const renderActivities = (
@@ -535,7 +365,7 @@ export const ProposalDetailsView = (props: {
     {
       id: "instruction",
       name: "Instruction",
-      render: renderInstructions
+      render: <RenderInstructions connection={connection} proposalIxInfo={proposalIxInfo} />
     }, 
     {
       id: "activity",
@@ -795,11 +625,6 @@ export const ProposalDetailsView = (props: {
             </div>
           </div>
         </div>
-
-        {/* <Row>
-          <h3 className="mt-1 proposal-instruction">Instruction</h3>
-          {renderInstructions}
-        </Row> */}
 
         <TabsMean
           tabs={tabs}
