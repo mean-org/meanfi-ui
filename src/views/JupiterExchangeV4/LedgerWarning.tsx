11b086141eef3fadb124668e1c64ada9b48351fb
Yamel Amador Fernandez <yamel_amador_fdez@msn.com>
2023-08-29 21:19:28 -0600

    Safe Tx Ix Decoder moved to the SDK

--- src/views/JupiterExchangeV4/LedgerWarning.tsx
+++ src/views/JupiterExchangeV4/LedgerWarning.tsx
@@ -1,4 +1,5 @@
-import { WarningFilled } from '@ant-design/icons';
+import { InfoCircleOutlined } from '@ant-design/icons';
+import { InfoIcon } from 'components/InfoIcon';
 import { useTranslation } from 'react-i18next';
 
 export const LedgerWarning = () => {
@@ -6,18 +7,11 @@ export const LedgerWarning = () => {
 
   return (
     <div className="mt-3">
-      <div data-show="true" className="ant-alert ant-alert-warning align-items-start" role="alert">
-        <span
-          role="img"
-          aria-label="exclamation-circle"
-          className="anticon anticon-exclamation-circle ant-alert-icon mt-1 mr-2"
-        >
-          <WarningFilled style={{ fontSize: '1.25rem' }} />
-        </span>
-        <div className="ant-alert-content">
-          <div className="ant-alert-message">{t('swap.ledger-upgrade-warning')}</div>
-          <div className="ant-alert-description"></div>
-        </div>
+      <div className="left flex-row justify-content-end align-items-center">
+        <div className="inner-label w-auto my-0">{t('swap.ledger-owner-notice')}</div>
+        <InfoIcon content={<span>{t('swap.ledger-upgrade-warning')}</span>} placement="bottom">
+          <InfoCircleOutlined style={{ lineHeight: 0 }} />
+        </InfoIcon>
       </div>
     </div>
   );
