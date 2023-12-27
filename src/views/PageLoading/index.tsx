c762e5f9f6fabfe7461784f60ac1260b99c2053b
Yamel Amador Fernandez <yamel_amador_fdez@msn.com>
2023-12-26 19:06:20 -0600

    Apply new brand to loading animation

--- src/views/PageLoading/index.tsx
+++ src/views/PageLoading/index.tsx
@@ -16,31 +16,17 @@ export const PageLoadingView = (props: { message?: string; addWrapper?: boolean 
                   xmlns="http://www.w3.org/2000/svg"
                   width="100%"
                   height="100%"
-                  viewBox="0 0 245 238"
+                  viewBox="0 0 1000 1000"
                   fillRule="evenodd"
                   clipRule="evenodd"
-                  strokeLinejoin="round"
-                  strokeMiterlimit="2"
                 >
                   <path
-                    d="M238.324 75l-115.818 30.654L6.689 75 0 128.402l47.946 122.08L122.515 313l74.55-62.518L245 128.402 238.324 75zm-21.414 29.042l3.168 25.313-42.121 107.268-26.849 22.511 37.922-120.286-48.471 12.465-8.881 107.524-9.176 24.128-9.174-24.128-8.885-107.524-48.468-12.465 37.922 120.286-26.85-22.511-42.118-107.268 3.167-25.313 94.406 24.998 94.408-24.998z"
-                    fill="url(#_Linear1)"
-                    transform="translate(0 -64)"
+                    className="lettermark"
+                    d="m621.92,327.07c-48.28,0-91.71,20.71-121.92,53.72-30.21-33.01-73.65-53.72-121.92-53.72-91.25,0-165.22,73.97-165.22,165.22v180.65h86.62v-180.81c0-43.41,35.19-78.6,78.6-78.6s78.6,35.19,78.6,78.6v180.81h86.64v-180.81c0-43.41,35.19-78.6,78.6-78.6s78.6,35.19,78.6,78.6v180.81h86.62v-180.65c0-91.25-73.97-165.22-165.22-165.22Z"
+                    fill="currentColor"
+                    strokeWidth="0px"
+                    transform="translate(0 -32)"
                   />
-                  <defs>
-                    <linearGradient
-                      id="_Linear1"
-                      x1="0"
-                      y1="0"
-                      x2="1"
-                      y2="0"
-                      gradientUnits="userSpaceOnUse"
-                      gradientTransform="matrix(0 238 -238 0 122.5 75)"
-                    >
-                      <stop offset="0" stopColor="#ff0017" />
-                      <stop offset="1" stopColor="#b7001c" />
-                    </linearGradient>
-                  </defs>
                 </svg>
               </div>
               <svg className="spinner" viewBox="25 25 50 50">
