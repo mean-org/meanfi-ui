423c9c96e003ab49e0c884646a891a497d472052
Yamel Amador Fernandez <yamel_amador_fdez@msn.com>
2024-12-25 21:09:33 -0600

    Fix streamList | Refactor perfSamples (#629)

--- src/query-hooks/performanceSamples/index.ts
+++ src/query-hooks/performanceSamples/index.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { FIVE_MINUTES_REFRESH_TIMEOUT } from 'src/app-constants/common';
 import { useConnection } from 'src/contexts/connection';
+import { isProd } from 'src/middleware/ui';
 import getPerformanceSamples from './getPerformanceSamples';
 
 export const getPerformanceSamplesKey = () => ['performance-samples'];
@@ -8,16 +9,12 @@ export const getPerformanceSamplesKey = () => ['performance-samples'];
 const useGetPerformanceSamples = () => {
   const connection = useConnection();
 
-  const { data } = useQuery({
+  return useQuery({
     queryKey: getPerformanceSamplesKey(),
     queryFn: () => getPerformanceSamples(connection),
-    enabled: !!connection,
+    enabled: !!connection && isProd(),
     refetchInterval: FIVE_MINUTES_REFRESH_TIMEOUT,
   });
-
-  return {
-    tpsAvg: data,
-  };
 };
 
 export default useGetPerformanceSamples;
